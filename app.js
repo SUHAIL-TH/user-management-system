66d9f701f3921bb0bee5ab8289aaf968031bf4c7
suhail th <suhailth17756@gmail.com>
2023-03-03 17:22:08 +0530

    2nd commit

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ const express=require("express")
 const  session=require("express-session")
 const cookiParser=require("cookie-parser")
 const path=require("path")
+const adminRouter=require("./routes/admin")
+const userRouter=require("./routes/user")
+const dotenv=require("dotenv")
 
 const app=express()
+app.set(cookiParser())
+app.set('views');
+app.set('view engine', 'ejs');
+app.use(express.static(patch.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: 6000000 },
+    resave: false 
+}));
+dotenv.config()
+//remove cache
+app.use((req, res, next) => {
+    res.header(
+      "Cache-Control",
+      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+    next();
+});
+
+app.use("/",userRouter)
+app.use("/admin",adminRouter)
+
+
 
 
 app.listen(3000,()=>{
